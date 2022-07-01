f44771192c41f296e9372937fcbeae46f79688d9
zhengjiabin@bestseller.com.cn <zhengjiabin@bestseller.com.cn>
2022-07-01 17:58:32 +0800

    1.提取三级分类为工具类 2.在属性分组中引用三级分类

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <el-tree :data="data" :props="defaultProps"  node-key="catId"></el-tree>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    data() {
+        return {
+            data: [],
+            defaultProps: {
+                children: 'childrens',
+                label: 'name'
+            }
+        };
+    },
+    methods: {
+        getCategory() {
+            this.$http({
+                url: this.$http.adornUrl('/product/category/listTree'),
+                method: 'get'
+            }).then(({ data }) => {
+                console.log("成功获取的类别数据: ", data.data)
+                this.data = data.data
+            });
+        }, 
+    },
+    created() {
+        this.getCategory();
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/modules/product/attrgroup.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-row :gutter="20">
+  <el-col :span="8">
+  <category></category>
+  </el-col>
+  <el-col :span="16"><div class="grid-content bg-purple">表格</div></el-col>
+</el-row>
+</template>
+
+<script>
+import category from '../common/category.vue'
+export default {
+    data(){
+
+    },components:{category}
+}
+</script>
+
+<style>
+
+</style>
